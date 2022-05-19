8d1b14eb7ad92e3333de7048057cae7f7ca34132
Noam Ross <ross@ecohealthalliance.org>
2022-05-19 14:43:55 -0400

    Fix dolt_diffs()

--- R/dolt-diffs.R
+++ R/dolt-diffs.R
@@ -2,12 +2,14 @@
 #'
 #' @param table [character] the name of a table in the database
 #' @param to commit to compare to
+#' @param from commit to compare from
 #' @inheritParams dolt_branches
 #' @export
 #' @rdname dolt-diffs
-dolt_diffs <- function(table, to, conn = dolt(), collect = NULL, show_sql = NULL) {
+dolt_diffs <- function(table, to, from, conn = dolt(), collect = NULL, show_sql = NULL) {
   collect <- .collect(collect); show_sql <- .show_sql(show_sql)
-  query <- paste0("select * from dolt_commit_diff_", table, " WHERE to_commit = ", to)
+  query <- paste0("select * from dolt_commit_diff_", table,
+                  " where to_commit='", to, "' and from_commit='", from, "'")
   dolt_query(query, conn, collect, show_sql)
 }
 
