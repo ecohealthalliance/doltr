0a3166eafca87f59f371383d2af0c81cb5b0f860
Nathan Layman <layman@ecohealthalliance.org>
2023-02-05 16:04:14 -0800

    Add in patch to dbxInsert that allows sf geometry columns to be inserted to mySQL db.

--- R/sf-read.R
+++ R/sf-read.R
@@ -13,13 +13,23 @@ sf_read <- function(conn, table_name = "us_state_capitals_NCL") {
   # Read raw table
   sf_via_dbi <- DBI::dbReadTable(conn, table_name)
 
+  # Identify geometry column. Sadly DBI::dbReadTable only returns 'char' for wkb columns
+  # This still needs to be resolved. For now in our tests it's the 3rd column
+  names(sf_via_dbi)[3] <- "coord"
+  geom_col <- names(sf_via_dbi)[3]
+
+  # This only works for text. lapply is uglier but works
   # Split raw into crs and wkb columns
+  # sf_via_dbi <- sf_via_dbi |> rowwise() |>
+  #   separate(all_of(geom_col), sep = 4, c("crs", "wkb"))
+
+  # This works for raw
   sf_via_dbi <- sf_via_dbi |>
     mutate(crs = lapply(coord, function(x) x[1:4]),
            wkb = lapply(coord, function(x) x[-c(1:4)])) |>
     select(-coord)
 
-  # Convert crs to int then use sf machinery to translate wkb
+  # This works for raw
   sf_via_dbi <- sf_via_dbi |>
     rowwise() |>
     mutate(crs = readBin(crs, what = "int", n = 1, size = 4L)) |>
@@ -33,14 +43,14 @@ sf_read <- function(conn, table_name = "us_state_capitals_NCL") {
   # Add in error protection for unkown crs
   sf_via_dbi <- possibly(st_set_crs, sf_via_dbi)(sf_via_dbi$crs[1]) |>
   select(-crs) |>
-  rename(!!st_geometry_type(sf_via_dbi, by_geometry = F) |> as.character() := wkb)
+  rename(!!geom_col := wkb)
 
   sf_via_dbi
 }
 
 
-
-#' Proposed mySQL sf_write method
+# NCL:I don't think we'll go this route yet. dbx is a better target to patch
+#' Proposed mySQL sf_write method.
 #'
 #' @param conn
 #' @param sfc
@@ -60,7 +70,8 @@ sf_write <- function(conn, sfc, table_name) {
   crs <- ifelse(is.na(crs), "0000", R.utils::intToHex(crs))
 
   # Identify geometry column
-  sf_col <- names(sfc)[which(map_lgl(map(sfc, class), ~any("sfc" %in% .x)))]
+  # sf_col <- names(sfc)[which(map_lgl(map(sfc, class), ~any("sfc" %in% .x)))]
+  sf_col <- "Point"
 
   # Get wkb hex string and append crs
   # This might need some work if there are multiple sf columns?
@@ -70,6 +81,10 @@ sf_write <- function(conn, sfc, table_name) {
 
   sfc
 
-  # To Do: Convert to blob and dbExecute. Re-use sf machinery?
+}
 
+# from: https://stackoverflow.com/questions/70884796/convert-hexadecimal-string-to-bytes-in-r
+hex_to_raw <- function(x) {
+  digits <- strtoi(strsplit(x, "")[[1]], base=16L)
+  as.raw(bitwShiftL(digits[c(TRUE, FALSE)],4) + digits[c(FALSE, TRUE)])
 }
